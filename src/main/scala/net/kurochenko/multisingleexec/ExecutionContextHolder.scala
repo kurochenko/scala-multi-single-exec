25d1f7b95c66d1e0cb8aaba79b08355d517c831d
Andrej Kuročenko <kurochenko@gmail.com>
2016-04-24 17:31:14 +0200

    Created execution context holder for single threaded execution contexts accessible by hash buckets.

--- src/main/scala/net/kurochenko/multisingleexec/ExecutionContextHolder.scala
+++ src/main/scala/net/kurochenko/multisingleexec/ExecutionContextHolder.scala
@@ -1,10 +1,41 @@
 package net.kurochenko.multisingleexec
 
+import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext
+
 
 trait ExecutionContextHolder {
 
   def size: Int
   def getExecutionContextForBucket(bucket: Bucket): ExecutionContext
+  
+}
 
+
+class ExecutionContextHolderImpl extends ExecutionContextHolder {
+
+  val ContextPoolSize: Int = 10
+
+  lazy val contexts = (0 to ContextPoolSize)
+    .toList
+    .map { i =>
+      Bucket(i) -> createExecutionContext
+    }.toMap
+
+
+  override val size: Int = ContextPoolSize
+
+
+  override def getExecutionContextForBucket(bucket: Bucket): ExecutionContext = {
+    if (bucket.number >= ContextPoolSize) {
+      throw new IllegalStateException(s"Bucket number ${bucket.number} exceeds allocated buckets count $ContextPoolSize")
+    }
+
+    contexts.get(bucket).get
+  }
+
+
+  private def createExecutionContext: ExecutionContext = {
+    ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
+  }
 }
