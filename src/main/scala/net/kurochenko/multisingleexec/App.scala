6a7996f26c24b573d6d25fa9f0c49518f7be5608
Andrej Kuročenko <kurochenko@gmail.com>
2016-04-24 17:33:45 +0200

    Simple app for evenly bucketing random hashes and creating single threaded execution contexts for every bucket.

--- src/main/scala/net/kurochenko/multisingleexec/App.scala
+++ src/main/scala/net/kurochenko/multisingleexec/App.scala
@@ -1,5 +1,35 @@
 package net.kurochenko.multisingleexec
 
-class App {
+import java.util.UUID
+
+
+object App {
+
+  def main(args: Array[String]) {
+    val hashChooser: HashBucketChooser = new HashBucketChooserImpl()
+    val executionContextHolder: ExecutionContextHolder = new ExecutionContextHolderImpl()
+
+    val bucketCount = executionContextHolder.size
+
+    val results = new java.util.HashMap()
+
+    val hash = "some hash"
+
+    (0 to 100000)
+      .toList
+      .map(_ => UUID.randomUUID().toString())
+      .map(uuid => hashChooser.getBucketForHash(uuid, bucketCount))
+      .groupBy(identity)
+      .mapValues(_.size)
+      .map { case (k,v) => (k,v)}
+      .toList
+      .sortBy{ case (k,v) => k.number}
+      .foreach { case (bucket, count) =>
+
+        println(s"${bucket.number}:\t$count")
+
+    }
+
+  }
 
 }
